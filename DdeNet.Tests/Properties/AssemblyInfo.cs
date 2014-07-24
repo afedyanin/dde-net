f6314130fc8b83131435c1081e710aeaccf80a6a
rootzid <rootzid@gmail.com>
2014-07-24 19:27:20 +0400

    Adding samples & tests

---
+++ DdeNet.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DdeNet.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DdeNet.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfff6c54-dbd7-425a-82ce-747908d0d71f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ServerSample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SampleServer
+{
+    using System;
+    using DdeNet.Server;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create a server that will register the service name 'myapp'.
+                using (DdeServer server = new MyServer("myapp"))
+                {
+                    // Register the service name.
+                    server.Register();
+
+                    // Wait for the user to press ENTER before proceding.
+                    Console.WriteLine("Press ENTER to quit...");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Press ENTER to quit...");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
