632772a69bd585bba1afa10b4a07c9407cb0a119
rootzid <rootzid@gmail.com>
2014-07-24 19:35:29 +0400

    Add tests

---
+++ DdeNet.Tests/Helpers/EventListener.cs
@@ -0,0 +1,29 @@
+namespace DdeNet.Test
+{
+    using System.Collections.Generic;
+    using DdeNet;
+
+    internal sealed class EventListener
+    {
+        private System.Threading.ManualResetEvent _Received = new System.Threading.ManualResetEvent(false);
+        private List<DdeEventArgs>                _Events   = new List<DdeEventArgs>();
+
+        public List<DdeEventArgs> Events
+        {
+            get { return _Events; }
+        }
+
+        public System.Threading.WaitHandle Received
+        {
+            get { return _Received; }
+        }
+
+        public void OnEvent(object sender, DdeEventArgs args)
+        {
+            _Events.Add(args);
+            _Received.Set();
+        }
+
+    } // class
+
+} // namespace
